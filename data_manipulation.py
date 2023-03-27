a6faeeb0da809ea516215d4b4f684f6f0f55ed86
Omar Torres <torres.omar@berkeley.edu>
2023-03-27 13:08:34 -0700

    commiting able to use python

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,4 +1,10 @@
-import pandas as pd
-import numpy as np
-
-print(np.pi)
+def get_filtered_cars(clean_df ,budget, fuelTypes, transmissionPreferences, drivetrainPreferences, minPreferedMPG):
+    filtered_df = clean_df[clean_df['Price'] < budget]
+    filtered_df = filtered_df[filtered_df["Fuel_Type"].isin(fuelTypes)]
+    filtered_df = filtered_df[filtered_df["Transmission"].isin(transmissionPreferences)]
+    filtered_df = filtered_df[filtered_df["Drivetrain"].isin(drivetrainPreferences)]
+    filtered_df = filtered_df[filtered_df['min_MPG'] > minPreferedMPG]
+    models = filtered_df['Model'].unique()
+    model_lst = list(models)
+    print(model_lst)
+    return model_lst
